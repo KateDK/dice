b1a4a0db29957a1dbd7eb9c71ced4c23cfbfbabe
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-29 14:24:29 -0400

    Create & render RollDice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import RollDice from './RollDice';
 function App() {
   return (
     <div className="App">
-
+      <RollDice/>
     </div>
   );
 }
---
+++ src/RollDice.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RollDice extends React.Component{
+  render(){
+    return(
+      <div className="RollDice">
+        Hello!
+      </div>
+    )
+  }
+}
+
+
+export default RollDice;
