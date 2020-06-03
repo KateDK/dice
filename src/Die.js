76228180752373f27e56c7ee689a9103218060df
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-03 15:25:32 -0400

    use rolling to set className

--- src/Die.js
+++ src/Die.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Die.css'
 
 const Die = (props)=>{
-  const {num} = props;
-  const cName = `fas fa-dice-${num}`
+  const {num, rolling} = props;
+  const dieName = `fas fa-dice-${num}`
+  const cName = rolling ? "Die rolling" : "Die"
   return (
-  <div className='Die'>
-    <i className={cName}></i>
+  <div className={cName}>
+    <i className={dieName}></i>
   </div>
   )
 }
